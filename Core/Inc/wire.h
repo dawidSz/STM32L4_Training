9474f4d9b9e56c7e8818f18533d7e29c9b647889
Dawid Szalwinski <dawid.szalwinski@gmail.com>
2026-07-30 11:58:40 +0200

    Part #17: Moved implementation of communication to separate libraries.

---
+++ Core/Inc/wire.h
@@ -0,0 +1,19 @@
+/*
+ * wire.h
+ *
+ *  Created on: 30 lip 2026
+ *      Author: dawid
+ */
+
+#ifndef INC_WIRE_H_
+#define INC_WIRE_H_
+
+#include "stm32l4xx.h"
+
+HAL_StatusTypeDef wire_init(void);
+HAL_StatusTypeDef wire_reset(void);
+uint8_t wire_read(void);
+void wire_write(uint8_t byte);
+uint8_t wire_crc(const uint8_t* data, int len);
+
+#endif /* INC_WIRE_H_ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,6 +26,7 @@
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include <stdlib.h>
+#include "ds18b20.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -56,98 +57,6 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-void delay_us(uint32_t us)
-{
-  __HAL_TIM_SET_COUNTER(&htim6, 0);
-  while (__HAL_TIM_GET_COUNTER(&htim6) < us) {}
-}
-
-HAL_StatusTypeDef wire_reset(void)
-{
-  int rc;
-  HAL_GPIO_WritePin(DS_GPIO_Port, DS_Pin, GPIO_PIN_RESET);
-  delay_us(480);
-  HAL_GPIO_WritePin(DS_GPIO_Port, DS_Pin, GPIO_PIN_SET);
-  delay_us(70);
-  rc = HAL_GPIO_ReadPin(DS_GPIO_Port, DS_Pin);
-  delay_us(410);
-  if (rc == 0)
-    return HAL_OK;
-  else
-    return HAL_ERROR;
-}
-
-void write_bit(int value)
-{
-  if (value) {
-    HAL_GPIO_WritePin(DS_GPIO_Port, DS_Pin, GPIO_PIN_RESET);
-    delay_us(6);
-    HAL_GPIO_WritePin(DS_GPIO_Port, DS_Pin, GPIO_PIN_SET);
-    delay_us(64);
-  } else {
-    HAL_GPIO_WritePin(DS_GPIO_Port, DS_Pin, GPIO_PIN_RESET);
-    delay_us(60);
-    HAL_GPIO_WritePin(DS_GPIO_Port, DS_Pin, GPIO_PIN_SET);
-    delay_us(10);
-  }
-}
-
-void wire_write(uint8_t byte)
-{
-  int i;
-  for (i = 0; i < 8; i++) {
-    write_bit(byte & 0x01);
-    byte >>= 1;
-  }
-}
-
-int read_bit(void)
-{
-  int rc;
-  HAL_GPIO_WritePin(DS_GPIO_Port, DS_Pin, GPIO_PIN_RESET);
-  delay_us(6);
-  HAL_GPIO_WritePin(DS_GPIO_Port, DS_Pin, GPIO_PIN_SET);
-  delay_us(9);
-  rc = HAL_GPIO_ReadPin(DS_GPIO_Port, DS_Pin);
-  delay_us(55);
-  return rc;
-}
-
-uint8_t wire_read(void)
-{
-  uint8_t value = 0;
-  int i;
-  for (i = 0; i < 8; i++) {
-    value >>= 1;
-    if (read_bit())
-      value |= 0x80;
-  }
-  return value;
-}
-
-uint8_t byte_crc(uint8_t crc, uint8_t byte)
-{
-  int i;
-  for (i = 0; i < 8; i++) {
-    uint8_t b = crc ^ byte;
-    crc >>= 1;
-    if (b & 0x01)
-      crc ^= 0x8c;
-    byte >>= 1;
-  }
-  return crc;
-}
-
-uint8_t wire_crc(const uint8_t* data, int len)
-{
-  int i;
-    uint8_t crc = 0;
-    for (i = 0; i < len; i++)
-      crc = byte_crc(crc, data[i]);
-    return crc;
-}
-
 int __io_putchar(int ch)
 {
   if (ch == '\n') {
@@ -188,24 +97,16 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_TIM6_Init();
+
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start(&htim6);
-  wire_reset();
-  wire_write(0xcc);
-  wire_write(0x44);
-  HAL_Delay(750);
-  wire_reset();
-  wire_write(0xcc);
-  wire_write(0xbe);
-  int i;
-  uint8_t scratchpad[9];
-  for (i = 0; i < 9; i++)
-    scratchpad[i] = wire_read();
-  uint8_t crc = wire_crc(scratchpad, 8);
-  /* USER CODE END 2 */
+  if (ds18b20_init() != HAL_OK) {
+    Error_Handler();
+  }
+  uint8_t ds1[DS18B20_ROM_CODE_SIZE];
 
-  /* USER CODE END 2 */
-
+  if (ds18b20_read_address(ds1) != HAL_OK) {
+    Error_Handler();
+  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -213,10 +114,18 @@ int main(void)
 
   while (1)
   {
+    ds18b20_start_measure(NULL);
+    HAL_Delay(750);
 
-    /* USER CODE END WHILE */
+    float temp = ds18b20_get_temp(NULL);
 
-    /* USER CODE BEGIN 3 */
+    if (temp >= 80.0f)
+      printf("Sensor error...\n");
+    else
+      printf("T1 = %.1f*C\n", temp);
+      /* USER CODE END WHILE */
+
+      /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
